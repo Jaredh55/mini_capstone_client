a84480049f437eb759498b5dcb64552f349d0643
Jaredh55 <jaredh55@gmail.com>
2018-05-14 21:35:23 -0500

    client routes

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,7 +1,58 @@
 class Client::ProductsController < ApplicationController
-  def action
+  def index
     response = Unirest.get("http://localhost:3000/api/animals")
     @animals = response.body
     render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                        name: params[:name],
+                        price: params[:price],
+                        description: params[:description],
+                        image_url: params[:image_url]
+                      }
+    response = Unirest.post(
+                            "http://localhost:3000/api/animals", parameters: client_params
+                            )
+    render 'create.html.erb'
+  end
+
+  def show
+    animal_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/animals/#{animal_id}")
+    @animal = response.body #hash
+    render 'show.html.erb'
+  end
+
+  def edit
+    @animal_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/animals/#{@animal_id}")
+    @animal = response.body
+    render 'edit.html.erb'
+  end
+
+    def update
+        client_params = {
+                          name: params[:name],
+                          price: params[:price],
+                          description: params[:description],
+                          image_url: params[:image_url]
+                      }
+     response = Unirest.patch(
+                             "http://localhost:3000/api/animals/#{ params[:id] }",
+                             parameters: client_params
+                            )
+        render 'update.html.erb'
+  end
+
+    def destroy
+    animal_id = params[:id]
+    response = Unirest.delete("http://localhost:3000/api/animals/#{ animal_id }")
+    render 'destroy.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get '/' => 'client/animals#action'
   namespace :client do
-    get '/animals' => 'products#action'
+    get '/animals' => 'products#index'
+    get '/animals/new' => 'products#new'
+    post '/animals' => 'products#create'
+    get '/animals/:id' => 'products#show'
+    get '/animals/:id/edit' => 'products#edit'
+    patch 'animals/:id' => 'products#update'
+    delete '/animals/:id' => 'products#destroy'
   end
 end
